f30abc165c7180372cbb5c449b443d9164dbc943
nick31n3-source <nick31.n3@gmail.com>
2025-11-24 22:40:07 -0300

    Implement handleConfirmation function

---
+++ handleConfirmation.js
@@ -0,0 +1,6 @@
+const handleConfirmation = async (bp, event, state) => {
+  const confirmation = event.text.toLowerCase();
+  state.confirmed = confirmation === 'sim';
+  return state;
+};
+module.exports = handleConfirmation;
